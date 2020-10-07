2f0659b837e361c1186902e460cd42b5bab57636
therealgherkhin <lscholz14@googlemail.com>
2020-10-07 09:53:10 +0200

    added berlin charlottenburg

--- app/routes.py
+++ app/routes.py
@@ -11,14 +11,47 @@ def index():
 
 @app.route('/berlin-lichtenberg')
 def berlinlichtenberg():
-    var = get_auslastung()
+    var = get_auslastung_lichtenberg()
     return render_template('berlin-lichtenberg.html', var=var)
 
+@app.route('/berlin-charlottenburg')
+def berlincharlottenburg():
+    var = get_auslastung_charlottenburg()
+    return render_template('berlin-charlottenburg.html', var=var)
 
 
-def get_auslastung():
+
+def get_auslastung_lichtenberg():
     try:
         url = "https://www.mcfit.com/de/fitnessstudios/studiosuche/studiodetails/studio/berlin-lichtenberg/"
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+        driver.get(url)
+
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, "lxml")
+
+        elems = soup.find_all('div', {'class': 'sc-iJCRLp eDJvQP'})
+        # print(elems)
+        auslastung = str(elems).split("<span>")[1]
+        # print(auslastung)
+        auslastung = auslastung[:auslastung.rfind('</span>')]
+        print(auslastung)
+
+        ergebnis = {'auslastung': auslastung}
+
+        return ergebnis
+
+    finally:
+        try:
+            driver.close()
+        except:
+            pass
+
+def get_auslastung_charlottenburg():
+    try:
+        url = "https://www.mcfit.com/de/fitnessstudios/studiosuche/studiodetails/studio/berlin-charlottenburg/"
         options = FirefoxOptions()
         options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
